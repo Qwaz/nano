c1c25027ddedb3c2af81185333aa9bd09d969e8a
Jinwoo Kim <kjw227@postech.ac.kr>
2016-05-06 06:40:53 -0700

    optimized and debugged cnn part a little

---
+++ cifar.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pickle
+
+from nano.connection.activation import ReLU, Linear, Sigmoid
+from nano.connection.dimensional_to_dimensional import Convolution, MaxPooling
+from nano.connection.dimensional_to_linear import Projection
+from nano.connection.linear_to_linear import FullyConnected
+from nano.layer import LinearLayer, DimensionalLayer
+import nano.network
+import nano.trainer
+
+convnet = nano.network.Network('input', 'output_a')
+
+convnet.add_layer('input', DimensionalLayer(3, 32, 32))
+convnet.add_layer('conv_1', DimensionalLayer(2, 31, 31))
+convnet.add_layer('conv_1a', DimensionalLayer(2, 31, 31))
+convnet.add_layer('pool_1', DimensionalLayer(2, 5, 5))
+convnet.add_layer('mlp_in', LinearLayer(20))
+convnet.add_layer('mlp_ina', LinearLayer(20))
+convnet.add_layer('output', LinearLayer(10))
+convnet.add_layer('output_a', LinearLayer(10))
+
+convnet.add_connection('input', 'conv_1', Convolution(2, 2))
+convnet.add_connection('conv_1', 'conv_1a', ReLU())
+convnet.add_connection('conv_1a', 'pool_1', MaxPooling(6, 6, 6))
+convnet.add_connection('pool_1', 'mlp_in', Projection())
+convnet.add_connection('mlp_in', 'mlp_ina', ReLU())
+convnet.add_connection('mlp_ina', 'output', FullyConnected())
+convnet.add_connection('output', 'output_a', Linear())
+
+tset = open('<path_to_cifar_dataset>', 'rb')
+rawdata = pickle.load(tset, encoding='bytes')
+
+train_set = []
+
+'''
+this is just an example for testing!
+you must change the data loading section to fully train the dataset
+'''
+for i in range(0, 300):
+	img = rawdata[b'data'][i]
+	ans = rawdata[b'labels'][i]
+
+	img = img / 255
+	img = img.reshape((3, 32, 32))
+	ansvec = np.zeros(10)
+	ansvec[ans] = 1
+
+	train_set.append((img, ansvec))
+
+tset.close()
+
+def log_result(current_epoch, network):
+    train_error = nano.trainer.rms(network, train_set)
+    print('EPOCH: %d - train error %g' % (current_epoch, train_error))
+
+sgd = nano.trainer.SGD(convnet)
+sgd.train(train_set, epoch=100, learning_rate=0.00002, epoch_func=log_result)
--- nano/connection/dimensional_to_dimensional.py
+++ nano/connection/dimensional_to_dimensional.py
@@ -1,207 +1,143 @@
 from abc import ABCMeta
 
 import numpy as np
-
+import math
 from nano.connection import AbsConnection
 from nano.layer import DimensionalLayer
-from nano.layer import LinearLayer
-
-class DimensionToDimension(AbsConnection, metaclass=ABCMeta):
-	def __init__(self):
-		super().__init__()
-
-	def check_layer_type(self, before_layer, after_layer):
-		return isinstance(before_layer, DimensionalLayer) and isinstance(after_layer, DimensionalLayer)
-
-class DimensionToLinear(AbsConnection, metaclass=ABCMeta):
-	def __init__(self):
-		super().__init__()
-
-	def check_layer_type(self, before_layer, after_layer):
-		return isinstance(before_layer, DimensionalLayer) and isinstance(after_layer, LinearLayer)
-
-class Convolution(DimensionToDimension):
-	def __init__(self, height, width, stride=1):
-		super().__init__()
-		self.stride = stride
-		self.height = height
-		self.width = width
-
-	def check_shape(self, before_shape, after_shape):
-		if ((before_shape[1] - self.height) // self.stride + 1 == after_shape[1]
-			and (before_shape[2] - self.width) // self.stride + 1 == after_shape[2]):
-			return True
-		else:
-			return False
-
-	def prepare_connection(self, before_shape, after_shape):
-		self.depth = before_shape[0]
-		self.filters = after_shape[0]
-		for i in range(0, self.filters):
-			self.weight.append(# filters
-				2 * np.random.random((self.depth, self.height, self.width)) - 1 
-			)
-		self.weight.append(# weights
-			np.random.random(self.filters)
-		)
-
-	def forward(self):
-		for i in range(0, self.filters):
-			filter_vec = self.weight[i].flatten()
-			bias = np.array([self.weight[self.filters][i]])
-
-			calc_matrix = np.zeros((self.height * self.width * self.depth , 1))
-			for j in range(0, self.after_layer.shape[1]):
-				for k in range(0, self.after_layer.shape[2]):
-					temp = np.zeros((1, 1))
-					for d in range(0, self.depth):
-						layer_values = self.before_layer.result[d, j * self.stride : j * self.stride + self.height, k * self.stride : k * self.stride + self.width]
-						layer_values = layer_values.reshape((self.height * self.width , 1))
-						temp = np.vstack((temp, layer_values))
-					temp = temp[1 : , :]
-					calc_matrix = np.hstack((calc_matrix, temp))
-
-			calc_matrix = calc_matrix[:, 1 : ]
-			res = np.dot(filter_vec, calc_matrix)
-			res = res + np.tile(bias, self.after_layer.shape[1] * self.after_layer.shape[2])
-			res = res.reshape((self.after_layer.shape[1], self.after_layer.shape[2]))
-			self.after_layer.result[i, :, :] = res
-		'''
-		for i in range(0, self.filters):
-			for j in range(0, self.after_layer.shape[1]):
-				for k in range(0, self.after_layer.shape[2]):
-					self.after_layer.result[i, k, j] += 
-					np.sum(numpy.multiply(self.before_layer.result[: ,self.stride * j : self.stride * j + self.height, self.stride * k : self.stride * k + self.width], self.weight[i]))
-					+ np.weight[self.filters][i]
-		'''
-	def backward(self):
-		'''
-		backprop currently supports only strides of size 1!!!
-		'''
-		back_error_prop = np.zeros((self.depth, self.before_layer.shape[1], self.before_layer.shape[2]))
-		for i in range(0, self.filters):
-			temp_weights = self.after_layer.error[i, :, :]
-			for j in range(0, self.height - 1):
-				temp_weights = np.insert(temp_weights, self.after_layer.shape[1], 0, axis=0)
-			for j in range(0, self.height - 1):
-				temp_weights = np.insert(temp_weights, 0, 0, axis=0)
-			for j in range(0, self.width - 1):
-				temp_weights = np.insert(temp_weights, self.after_layer.shape[2], 0, axis=1)
-			for j in range(0, self.width - 1):
-				temp_weights = np.insert(temp_weights, 0, 0, axis=1)
-			# zero padding for size of matrix
-			'''
-			for j in range(0, self.after_layer.shape[1]):
-				for k in range(0, self.stride - 1):
-					temp_weights = np.insert(temp_weights, self.height + i * self.stride, 0, axis=0)
-			for j in range(0, self.after_layer.shape[2]):
-				for k in range(0, self.stride - 1):
-					temp_weights = np.insert(temp_weights, self.width + i * self.stride, 0, axis=1)
-			#zero padding complete
-			'''
-			error_matrix = np.zeros((self.height * self.width, 1))
-			for j in range(0, self.before_layer.shape[1]):
-				for k in range(0, self.before_layer.shape[2]):
-					temp = temp_weights[j * self.stride : j * self.stride + self.height , k * self.stride : k * self.stride + self.width]
-					temp = temp.reshape(self.height * self.width, 1)
-					error_matrix = np.hstack((error_matrix, temp))
-
-			error_matrix = error_matrix[:, 1 : ]
-			#create the matrix of errors		
-
-			for j in range(0, self.depth):
-				filter_vec = self.weight[i][j, :, :].flatten()
-				filter_vec = filter_vec[: : -1]
-				back_error = np.dot(filter_vec, error_matrix)
-				back_error = back_error.reshape(self.before_layer.shape[1], self.before_layer.shape[2])
-				back_error_prop[j, :, :] += back_error
 
 
-			#reverse the filter vector and propagate the backwards error term
-		self.before_layer.error = back_error_prop
+class DimensionalToDimensional(AbsConnection, metaclass=ABCMeta):
+    def __init__(self):
+        super().__init__()
 
-		for i in range(0, self.filters):
-			filter_vec = self.after_layer.error[i, :, :] # this is the filter for the convolution
-			filter_vec = filter_vec.flatten() # we flatten it
-			bias_update = filter_vec.sum()
-			
-			calc_matrix = np.zeros((self.after_layer.shape[1] * self.after_layer.shape[2], 1))
-			for d in range(0, self.depth):
-				for j in range(0, self.height):
-					for k in range(0, self.width):
-						temp = np.zeros((1, 1))
-						layer_values = self.before_layer.result[d, j * self.stride : j * self.stride + self.after_layer.shape[1], k * self.stride : k * self.stride + self.after_layer.shape[2]]
-						layer_values = layer_values.reshape(self.after_layer.shape[1] * self.after_layer.shape[2], 1)
-						temp = np.vstack((temp, layer_values))
-						temp = temp[1 : , :]
-						calc_matrix = np.hstack((calc_matrix, temp))
+    def check_layer_type(self, before_layer, after_layer):
+        return isinstance(before_layer, DimensionalLayer) and isinstance(after_layer, DimensionalLayer)
 
-			calc_matrix = calc_matrix[:, 1 : ]
-			error_matrix = np.dot(filter_vec, calc_matrix)
-			error_matrix = error_matrix.reshape(self.depth, self.height, self.width)
+class Convolution(DimensionalToDimensional):
+    def __init__(self, height, width, stride=1):
+        super().__init__()
+        self.stride = stride
+        self.height = height
+        self.width = width
 
-			np.copyto(self.dweight[i], error_matrix) #update filter
-			self.dweight[self.filters][i] = bias_update
+    def check_shape(self, before_shape, after_shape):
+        if ((before_shape[1] - self.height) // self.stride + 1 == after_shape[1]
+            and (before_shape[2] - self.width) // self.stride + 1 == after_shape[2]):
+            return True
+        else:
+            return False
 
-class MaxPooling(DimensionToDimension):
-	def __init__(self, height, width, stride):
-		super().__init__()
-		self.height = height
-		self.width = width
-		self.stride = stride
+    def prepare_connection(self, before_shape, after_shape):
+        self.depth = before_shape[0]
+        self.filters = after_shape[0]
+        for i in range(self.filters):
+            self.weight.append(# filters
+                (2 * np.random.random((self.depth, self.height, self.width)) - 1) * math.sqrt(2 / (before_shape[0] * before_shape[1] * before_shape[2])) #intialization for ReLU, change if needed
+            )
+        self.weight.append(# biases
+            (2 * np.random.random(self.filters) - 1) * math.sqrt(2 / (before_shape[0] * before_shape[1] * before_shape[2]))
+        )
 
-	def check_shape(self, before_shape, after_shape):
-		if (before_shape[0] == after_shape[0]
-			and (before_shape[1] - self.height) // self.stride + 1 == after_shape[1]
-			and (before_shape[2] - self.width) // self.stride + 1 == after_shape[2]):
-			return True
-		else:
-			return False
-	def prepare_connection(self, before_shape, after_shape):
-		pass
-	def forward(self):
-		for i in range(0, self.after_layer.shape[0]):
-			for j in range(0, self.after_layer.shape[1]):
-				for k in range(0, self.after_layer.shape[2]):
-					self.after_layer.result[i, j, k] = (
-						np.amax(self.before_layer.result[i, self.stride * j : self.stride * j + self.height, self.stride * k : self.stride * k + self.width]))
+    def forward(self):
+        for i in range(self.filters):
+            filter_vec = self.weight[i].flatten()
+            bias = np.array([self.weight[self.filters][i]])
 
-	def backward(self):
-		self.before_layer.error.fill(0)
-		for i in range(0, self.after_layer.shape[0]):
-			for j in range(0, self.after_layer.shape[1]):
-				for k in range(0, self.after_layer.shape[2]):
-					ind = np.unravel_index(np.argmax(self.before_layer.result[i, self.stride * j : self.stride * j + self.height, self.stride * k : self.stride * k + self.width]), (1, self.height, self.width))
-					d = ind[0] + i
-					h = ind[1] + self.stride * j
-					w = ind[2] + self.stride * k
-					self.before_layer.error[(d, h, w)] = self.after_layer.error[i, j, k]
+            calc_matrix = np.zeros((self.height * self.width * self.depth , self.after_layer.shape[1] * self.after_layer.shape[2]))
+            for j in range(self.after_layer.shape[1]):
+                for k in range(self.after_layer.shape[2]):
+                    temp = np.zeros((self.height * self.width * self.depth, 1))
+                    for d in range(self.depth):
+                        layer_values = self.before_layer.result[d, j * self.stride : j * self.stride + self.height, k * self.stride : k * self.stride + self.width]
+                        layer_values = layer_values.reshape((self.height * self.width , 1))
+                        temp[d * self.height * self.width : (d + 1) * self.height * self.width, :] = layer_values    
+                    calc_matrix[:, j * self.after_layer.shape[2] + k : j * self.after_layer.shape[2] + k + 1] = temp
 
-class Projection(DimensionToLinear):
-	def __init__(self):
-		super().__init__()
-	def check_shape(self, before_shape, after_shape):
-		return True
-	def prepare_connection(self, before_shape, after_shape):
-		self.weight.append(
-			2 * np.random.random((before_shape[0] * before_shape[1] * before_shape[2], after_shape[1])) - 1
-		)
-		self.weight.append(
-			np.random.random(after_shape[1])
-		)
-	def forward(self):
-		input_temp = self.before_layer.result.flatten()
-		self.after_layer.result += np.dot(input_temp, self.weight[0]) + self.weight[1]
+            res = np.dot(filter_vec, calc_matrix)
+            res[:] = res + np.tile(bias, self.after_layer.shape[1] * self.after_layer.shape[2])
+            res = res.reshape((self.after_layer.shape[1], self.after_layer.shape[2]))
+            self.after_layer.result[i, :, :] += res    
 
-	def backward(self):
+    def backward(self):
+        '''
+        backprop currently supports only strides of size 1!!!
+        '''
+        back_error_prop = np.zeros((self.depth, self.before_layer.shape[1], self.before_layer.shape[2]))
+        # zero padding for size of matrix
+        for i in range(self.filters):
+            temp_weights = np.zeros((self.after_layer.shape[1] + (self.height - 1) * 2, self.after_layer.shape[2] * (self.width - 1) * 2))
+            temp_weights[self.height - 1 : self.height + self.after_layer.shape[1] - 1, self.width - 1 : self.width + self.after_layer.shape[2] - 1] = self.after_layer.error[i, :, :]
+            #create the matrix of errors 
+            error_matrix = np.zeros((self.height * self.width, self.before_layer.shape[1] * self.before_layer.shape[2]))
 
-		dw = np.repeat(np.array([self.before_layer.result.flatten()]).T, self.after_layer.shape[1], axis=1)
-		dw = np.multiply(dw, self.after_layer.error)
-		np.copyto(self.dweight[0], dw)
-		np.copyto(self.dweight[1], self.after_layer.error)
+            for j in range(self.before_layer.shape[1]):
+                for k in range(self.before_layer.shape[2]):
+                    temp = temp_weights[j * self.stride : j * self.stride + self.height , k * self.stride : k * self.stride + self.width]
+                    temp = temp.reshape(self.height * self.width, 1)
+                    error_matrix[:, j * self.before_layer.shape[2] + k : j * self.before_layer.shape[2] + k + 1] = temp
 
-		de = np.multiply(self.weight[0], self.after_layer.error)
-		de = np.sum(de, axis=1)
-		de = np.reshape(de, self.before_layer.shape)
-		np.copyto(self.before_layer.error, de)
-		
+            for j in range(self.depth):
+                filter_vec = self.weight[i][j, :, :].flatten()
+                filter_vec = filter_vec[: : -1]
+                back_error = np.dot(filter_vec, error_matrix)
+                back_error = back_error.reshape(self.before_layer.shape[1], self.before_layer.shape[2])
+                back_error_prop[j, :, :] += back_error
+
+        self.before_layer.error[:] = back_error_prop #this transmits the error
+
+        for i in range(self.filters):
+            filter_vec = self.after_layer.error[i, :, :] # this is the filter for the convolution (backprop uses the error as a convolution filter)
+            filter_vec = filter_vec.flatten() # we flatten it
+            bias_update = filter_vec.sum()
+            
+            calc_matrix = np.zeros((self.after_layer.shape[1] * self.after_layer.shape[2], self.depth * self.height * self.width))
+            temp = np.zeros((self.after_layer.shape[1] * self.after_layer.shape[2], 1))
+            for d in range(self.depth):
+                for j in range(self.height):
+                    for k in range(self.width):
+                        layer_values = self.before_layer.result[d, j * self.stride : j * self.stride + self.after_layer.shape[1], k * self.stride : k * self.stride + self.after_layer.shape[2]]
+                        layer_values = layer_values.reshape(self.after_layer.shape[1] * self.after_layer.shape[2], 1)
+                        temp = layer_values
+                        calc_matrix[:, d * self.height * self.width + j * self.width + k : d * self.height * self.width + j * self.width + k + 1] = temp
+
+            error_matrix = np.dot(filter_vec, calc_matrix)
+            error_matrix = error_matrix.reshape(self.depth, self.height, self.width)
+            self.dweight[i] = error_matrix #update filter
+            self.dweight[self.filters][i] = bias_update
+
+class MaxPooling(DimensionalToDimensional):
+    def __init__(self, height, width, stride):
+        super().__init__()
+        self.height = height
+        self.width = width
+        self.stride = stride
+
+    def check_shape(self, before_shape, after_shape):
+        if (before_shape[0] == after_shape[0]
+            and (before_shape[1] - self.height) // self.stride + 1 == after_shape[1]
+            and (before_shape[2] - self.width) // self.stride + 1 == after_shape[2]):
+            return True
+        else:
+            return False
+
+    def prepare_connection(self, before_shape, after_shape):
+        pass
+
+    def forward(self):
+        for i in range(self.after_layer.shape[0]):
+            for j in range(self.after_layer.shape[1]):
+                for k in range(self.after_layer.shape[2]):
+                    self.after_layer.result[i, j, k] = (
+                        np.amax(self.before_layer.result[i, self.stride * j : self.stride * j + self.height, self.stride * k : self.stride * k + self.width]))
+
+    def backward(self):
+        self.before_layer.error.fill(0)
+        for i in range(self.after_layer.shape[0]):
+            for j in range(self.after_layer.shape[1]):
+                for k in range(self.after_layer.shape[2]):
+                    ind = np.unravel_index(np.argmax(self.before_layer.result[i, self.stride * j : self.stride * j + self.height, self.stride * k : self.stride * k + self.width]), (1, self.height, self.width))
+                    d = ind[0] + i
+                    h = ind[1] + self.stride * j
+                    w = ind[2] + self.stride * k
+                    self.before_layer.error[d, h, w] = self.after_layer.error[i, j, k]
+        
